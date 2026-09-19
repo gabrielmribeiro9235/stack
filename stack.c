084a2f70a48e68656462b751003562059cfee565
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-19 10:26:11 -0300

    feat: create the top function

--- stack.c
+++ stack.c
@@ -56,3 +56,12 @@ int pop(t_stack *stack, int *value) {
     *value = stack->items[stack->top_index--];
     return 1;
 }
+
+int top(t_stack *stack, int *value) {
+    if (stack == NULL || value == NULL || stack->top_index == -1) {
+        return 0;
+    }
+
+    *value = stack->items[stack->top_index];
+    return 1;
+}
