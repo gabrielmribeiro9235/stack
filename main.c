96cc848ae37e9ad3a2df254bd27496a2f0bd909b
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-19 11:08:29 -0300

    feat: create the menu function in the main file

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
 #include"stack.h"
 
+void menu() {
+    printf("-----------------------------------------------\n");
+    printf("Select a function:\n");
+    printf("1 - print\n");
+    printf("2 - push\n");
+    printf("3 - pop\n");
+    printf("4 - top\n");
+    printf("5 - clear\n");
+    printf("6 - is_empy\n");
+    printf("7 - is_full\n");
+    printf("8 - size\n");
+    printf("9 - exit\n");
+    printf("-----------------------------------------------\n");
+    printf("Your choice: ");
+}
+
 int main() {
 
     return 0;
